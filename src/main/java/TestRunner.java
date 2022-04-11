725aac8b11378775efb908ced38f23428d87865d
Diana <dianapruteanu@ymail.com>
2022-04-11 17:27:26 +0300

    RO: Am setat managerul responsabil de gestiunea web driver-ului Selenium prun utilizarea dependențelor disponibile.
    ENG: Add first project dependencies and define the WebDriverManager.

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,2 +1,18 @@
+import managers.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestRunner {
+    public static void main(String[] args) {
+
+        WebDriverManager fireFoxDriveManager = new WebDriverManager("FireFox");
+        fireFoxDriveManager.getDriverul().get("https://www.google.com/");
+        fireFoxDriveManager.getDriverul().close();
+
+
+//        WebDriverManager webDriverManagerObiect = new WebDriverManager("Chrome");
+//        System.out.println(webDriverManagerObiect.getDriverul());
+//        webDriverManagerObiect.getDriverul().get("https://www.google.com/");
+//        webDriverManagerObiect.getDriverul().close();
+    }
 }
---
+++ src/main/java/managers/WebDriverManager.java
@@ -0,0 +1,38 @@
+package managers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Locale;
+import java.util.Objects;
+
+public class WebDriverManager {
+    public WebDriverManager(String browserType) {
+        this.browserType = browserType;
+    }
+
+    private WebDriver driverul;
+
+    private String browserType;
+
+    private void initializareDriver() {
+        if (Objects.equals(browserType.toUpperCase(Locale.ROOT), "CHROME")) {
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver.exe");
+            driverul = new ChromeDriver();
+        } else if (Objects.equals(browserType.toUpperCase(Locale.ROOT), "FIREFOX")) {
+            System.setProperty("webdriver.gecko.driver", "src/main/resources/drivers/geckodriver.exe");
+            driverul = new FirefoxDriver();
+        } else {
+            System.out.println("Nu exista un astfel de browser implementat!");
+        }
+    }
+
+    public WebDriver getDriverul() {
+        if (driverul == null) {
+            System.out.println("Se initializeaza browser-ul dorit");
+            initializareDriver();
+        }
+        return this.driverul;
+    }
+}
